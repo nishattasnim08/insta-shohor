85b0e2b1c6ea6627b44babf9f5d07c94a1e76953
nishattasnim08 <nishattasnim24258@gmail.com>
2022-03-11 18:14:49 +0600

    final correction commited

--- js/app.js
+++ js/app.js
@@ -39,8 +39,10 @@ const switchTab = (id) => {
         document.getElementById( "reported" ).textContent = "";
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
+        document.getElementById( "liked" ).textContent = "";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "reported" ).textContent = "";
 
         displayLikedPosts();
     } else {
